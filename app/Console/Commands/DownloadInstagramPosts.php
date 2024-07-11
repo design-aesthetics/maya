fbfced1842974fc858d1ee6e272300686a9f5912
Marco <marcomarocho@yahoo.ca>
2024-07-11 12:44:20 -0400

    updated python script logic

--- app/Console/Commands/DownloadInstagramPosts.php
+++ app/Console/Commands/DownloadInstagramPosts.php
@@ -11,11 +11,12 @@ class DownloadInstagramPosts extends Command
 
     public function handle()
     {
-        $prodPythonScript = storage_path('app/scripts/instagram_downloader.py');
-        $localPythonScript = storage_path('app/scripts/instagram_downloader.py');
+        $prodPythonScript = storage_path('app/scripts/prod_instagram_downloader.py');
+        $localPythonScript = storage_path('app/scripts/local_instagram_downloader.py');
 
         // Determine the Python interpreter based on the environment
         if (app()->environment('production')) {
+            $this->info('Running Instagram Production Downloader');
             $pythonInterpreter = "/app/venv/bin/python";
             $command = "{$pythonInterpreter} {$prodPythonScript}";
             $output = shell_exec($command);
@@ -23,6 +24,7 @@ class DownloadInstagramPosts extends Command
             $this->info($output);
         } else {
             $pythonInterpreter = "python3";
+            $this->info('Running Instagram Development Downloader');
             $command = "{$pythonInterpreter} {$localPythonScript}";
             $output = shell_exec($command);
             $this->info('Instagram posts downloaded successfully.');
