457ae1147a58acd63da4203f834199ed3c219e65
Marco <marcomarocho@yahoo.ca>
2024-07-14 17:38:54 -0400

    treatment styling edits

--- resources/views/partials/product-list.blade.php
+++ resources/views/partials/product-list.blade.php
@@ -3,13 +3,19 @@
 		<p>No products found</p>
 	@else
 		@foreach ($products as $product)
-			<div class="group relative overflow-hidden">
-				<img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="product-image h-full w-full object-cover transition-transform duration-300 group-hover:scale-105" />
-				<div class="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-black/50 via-black/25 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-					<div class="absolute inset-x-0 bottom-0 translate-y-full p-6 transition-transform duration-300 group-hover:translate-y-0">
-						<p class="prose-p prose text-white">{{ $product->brand->name }}</p>
-						<p class="prose-h1 prose prose-xl font-semibold text-white">{{ $product->name }}</p>
+			<div class="group relative flex h-full flex-col overflow-hidden">
+				<div class="relative flex-grow lg:h-[70vh]">
+					<img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="product-image h-full w-full object-cover transition-transform duration-300 group-hover:scale-105" />
+					<div class="absolute inset-x-0 bottom-0 hidden h-full bg-gradient-to-t from-black/50 via-black/25 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100 md:block">
+						<div class="absolute inset-x-0 bottom-0 translate-y-full p-6 transition-transform duration-300 group-hover:translate-y-0">
+							<p class="prose-p prose text-white">{{ $product->brand->name }}</p>
+							<p class="prose-h1 prose prose-xl font-semibold text-white">{{ $product->name }}</p>
+						</div>
 					</div>
+				</div>
+				<div class="bg-white p-4 md:hidden">
+					<p class="prose-p prose">{{ $product->brand->name }}</p>
+					<p class="prose-h1 prose">{{ $product->name }}</p>
 				</div>
 			</div>
 		@endforeach
--- resources/views/products/brand.blade.php
+++ resources/views/products/brand.blade.php
@@ -4,16 +4,19 @@
 
 	<section>
 		<div class="container">
-			<h2 class="mb-2 font-semplicita text-h2 font-medium uppercase text-primary-blue-5 lg:mb-3 lg:text-h1">
-				{{ $currentBrand ? $currentBrand->name . " Products" : "All Products" }}
-			</h2>
-			<p class="mx-auto pb-4 text-gray-500 lg:my-3">
-				@if ($currentBrand)
-					Discover our range of {{ $currentBrand->name }} products designed to enhance your beauty and wellness.
-				@else
-					Discover our range of products designed to enhance your beauty and wellness.
-				@endif
-			</p>
+			<div class="items-start text-left">
+				<h1 class="prose prose-lg mb-2 uppercase text-primary-blue-5 lg:mb-3">
+					{{ $currentBrand ? $currentBrand->name . " Products" : "All Products" }}
+				</h1>
+				<p class="prose pb-4 text-gray-500 lg:my-3">
+					@if ($currentBrand)
+						Discover our range of {{ $currentBrand->name }} products designed to enhance your beauty and wellness.
+					@else
+						Discover our range of products designed to enhance your beauty and wellness.
+					@endif
+				</p>
+			</div>
+
 			{{-- header filter and search component  --}}
 			<x-product-filter :brands="$brands" :categories="$categories" :currentBrand="$currentBrand" :currentCategory="$currentCategory" />
 		</div>
